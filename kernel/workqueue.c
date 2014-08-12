bca8c4648b8fa361f20dc59c04a5bfd19343e7fd
hellsgod <hellsgod@gmx.ch>
2014-08-12 13:35:01 +0200

    workqueue: Fix section mismatch

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -3473,7 +3473,7 @@ static int workqueue_cpu_up_callback(struct notifier_block *nfb,
  * Workqueues should be brought down after normal priority CPU notifiers.
  * This will be registered as low priority CPU notifier.
  */
-static int __devinit workqueue_cpu_down_callback(struct notifier_block *nfb,
+static int workqueue_cpu_down_callback(struct notifier_block *nfb,
 						 unsigned long action,
 						 void *hcpu)
 {
