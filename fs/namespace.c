666520bd1b8d2b8cba9d28557f3085cfa698c4dc
hellsgod <hellsgod@gmx.ch>
2015-09-05 20:49:02 +0200

    Revert "fs: namespace: fix build error"
    This reverts commit e3ca5372de20b5cf07291b5e79886eb6eed0065d

--- fs/namespace.c
+++ fs/namespace.c
@@ -2589,9 +2589,9 @@ void kern_unmount(struct vfsmount *mnt)
 {
 	/* release long term mount so mount point can be released */
 	if (!IS_ERR_OR_NULL(mnt)) {
-		br_write_lock(&vfsmount_lock);
+		br_write_lock(vfsmount_lock);
 		real_mount(mnt)->mnt_ns = NULL;
-		br_write_unlock(&vfsmount_lock);
+		br_write_unlock(vfsmount_lock);
 		mntput(mnt);
 	}
 }
